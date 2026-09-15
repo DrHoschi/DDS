210c6d239360c0a7076385bec0b6251d8072a8aa
DrHoschi <mann_alexander2000@yahoo.de>
2026-09-15 10:28:36 +0200

    Add files via upload

--- src/dds-04f/browser-app.mjs
+++ src/dds-04f/browser-app.mjs
@@ -2,17 +2,17 @@ import {
   ConstructionPrototypeController,
   MATERIAL_LABELS,
   PIECE_LABELS,
-} from "./construction-ui-controller.mjs?build=DDS-05B-TB1.5";
-import { loadConstructionAtlas } from "../dds-05a/atlas-loader.mjs?build=DDS-05B-TB1.5";
+} from "./construction-ui-controller.mjs?build=DDS-05B-TB1.6";
+import { loadConstructionAtlas } from "../dds-05a/atlas-loader.mjs?build=DDS-05B-TB1.6";
 import {
   createSceneProjection,
   projectWorldPoint,
   renderDepth,
   snapWorldPosition,
   spriteDescriptor,
-} from "../dds-05a/sprite-presentation.mjs?build=DDS-05B-TB1.5";
+} from "../dds-05a/sprite-presentation.mjs?build=DDS-05B-TB1.6";
 
-const EXPECTED_BUILD_ID = "DDS-05B-TB1.5";
+const EXPECTED_BUILD_ID = "DDS-05B-TB1.6";
 const activeBuildId = new URL(import.meta.url).searchParams.get("build");
 const buildIdValid = activeBuildId === EXPECTED_BUILD_ID;
 
